d95ccc49fe5edb2cfec7923a3aefa74bda1c534d
lo7656 <vladleonchik@mail.ru>
2020-06-02 21:18:58 +0300

    add navbar to ceilings

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,15 +58,36 @@ function change() {
 
 }
 
-// $(document).ready(function () {
-//     change()
+$(document).ready(function () {
+    change()
 
-//     $('.types__list-button').on('click', function () {
-//         let modal = document.querySelector('.modal')
+    $('.open').on('click', function () {
+        
+        let modal = document.querySelector('.modal')
+        let nav = document.querySelector('.navbar')
+        let icons = document.querySelectorAll('.navbar__icons-icon')
+        let body = document.querySelector('body')
+        let logo = document.querySelector('.navbar__logo-img')
+       
 
-//         if (modal.style.display == 'none')
-//             modal.style.display = 'block'
-//         else
-//             modal.style.display = 'none'
-//     })
-// })
+
+        if (modal.style.display == 'block'){
+            modal.style.display = 'none'
+            nav.style.backgroundColor = '#1D1D1D'
+            icons[1].src = 'assets/img/menu-white.png'
+            icons[0].src = 'assets/img/mail-white.png'
+            logo.src = 'assets/img/logo-white.png'
+            body.style.marginTop = '0'
+
+        }
+        else{
+            modal.style.display = 'block'
+            nav.style.backgroundColor = '#FFFFFF'
+            icons[1].src = 'assets/img/menu-dark.png'
+            icons[0].src = 'assets/img/mail-dark.png'
+            logo.src = 'assets/img/logo-dark.png'
+            body.style.marginTop = '521px'
+        }
+            
+    })
+})
