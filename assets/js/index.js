83384b0c77ec2e3e957a62855268d39efb0afd24
lo7656 <vladleonchik@mail.ru>
2020-06-17 13:51:59 +0300

    add  animate to answers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,26 @@
 $('.slider').slick({
-  dots: true,
-  centerMode: true,
-  arrows: false,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear',
-  autoplay: true,
-  autoplaySpeed: 2000
+    dots: true,
+    centerMode: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000
 })
+
+
+document.querySelectorAll('.answers__card-title').forEach(e => {
+    let state = false
+
+    e.addEventListener('click', () => {
+        state = !state
+        e.parentElement.classList.toggle('active')
+    })
+
+})
+
+// $(document).ready(function () {
+
+// })
